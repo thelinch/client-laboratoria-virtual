941010c6a928818375a44d2828e4760cfc269a2b
kerberos <kerberos.team.2019@gmail.com>
2019-10-26 11:12:52 -0500

    first commit

---
+++ src/app/global/global.module.ts
@@ -0,0 +1,136 @@
+import { NgModule } from "@angular/core";
+import {
+  MatAutocompleteModule,
+  MatBadgeModule,
+  MatBottomSheetModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule
+} from "@angular/material";
+import { A11yModule } from "@angular/cdk/a11y";
+import { BidiModule } from "@angular/cdk/bidi";
+import { ObserversModule } from "@angular/cdk/observers";
+import { OverlayModule } from "@angular/cdk/overlay";
+import { PlatformModule } from "@angular/cdk/platform";
+import { PortalModule } from "@angular/cdk/portal";
+import { ScrollDispatchModule } from "@angular/cdk/scrolling";
+import { CdkStepperModule } from "@angular/cdk/stepper";
+import { CdkTableModule } from "@angular/cdk/table";
+import { CdkTreeModule } from "@angular/cdk/tree";
+import { DragDropModule } from "@angular/cdk/drag-drop";
+import * as _moment from "moment";
+// tslint:disable-next-line:no-duplicate-imports
+import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE
+} from "@angular/material/core";
+import {
+  MAT_MOMENT_DATE_FORMATS,
+  MomentDateAdapter,
+  MAT_MOMENT_DATE_ADAPTER_OPTIONS,
+  MatMomentDateModule
+} from "@angular/material-moment-adapter";
+import { CustomDateAdapter } from "./services/customDateAdapter.service";
+
+const moment = _moment;
+const APP_DATE_FORMATS = {
+  parse: {
+    dateInput: "DD/MM/YYYY"
+  },
+  display: {
+    dateInput: "MM/DD/YYYY",
+    monthYearLabel: "MM YYYY",
+    dateA11yLabel: "MM/DD/YYYY",
+    monthYearA11yLabel: "MM YYYY"
+  }
+};
+/**
+ * NgModule that includes all Material modules.
+ */
+@NgModule({
+  exports: [
+    // CDK
+    A11yModule,
+    BidiModule,
+    ObserversModule,
+    OverlayModule,
+    PlatformModule,
+    PortalModule,
+    ScrollDispatchModule,
+    CdkStepperModule,
+    CdkTableModule,
+    CdkTreeModule,
+    DragDropModule,
+    // Material
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatStepperModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatMomentDateModule,
+    MatTreeModule,
+    MatNativeDateModule
+  ],
+  providers: [
+    CustomDateAdapter,
+    { provide: DateAdapter, useClass: CustomDateAdapter }
+  ]
+})
+export class MaterialModule {}
